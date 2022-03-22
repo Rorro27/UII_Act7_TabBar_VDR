8d69bccb4a701288e4fa8fedcef6118feef1d747
FlutLab.io <>
2022-03-22 22:53:36 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: const Icon(Icons.flight)),
+              Tab(icon: const Icon(Icons.directions_transit)),
+              Tab(icon: const Icon(Icons.directions_car)),
+            ],
+          ),
+          title: Text('Tabs Demo'),
+        ),
+        body: TabBarView(
+          children: [
+            Icon(Icons.flight, size: 350),
+            Icon(Icons.directions_transit, size: 350),
+            Icon(Icons.directions_car, size: 350),
+          ],
+        ), //TabBarView
+      ), // Scaffold
+    );
+  }
+}
